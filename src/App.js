7a27512af8037ef0a8395d734ac7a6765ca3264a
amin-mochtar <aminmr.mochtar0123@gmail.com>
2021-01-22 05:57:15 +0700

    component-home

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from "./components/my-navbar/navbar.component"
+import Carousel from "./components/my-carusol /Carousel.component"
+// import TextTitle from "./components/title-message/textTitle.component"
+import MyTitleMessage from "./components/title-message/title-message.component";
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <Carousel />
+      <MyTitleMessage />
     </div>
   )
 }
